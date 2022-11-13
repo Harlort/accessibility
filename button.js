50f5b089041ac133d96ff8ba97f64b43a66462e6
Christian <cm50@msn.com>
2022-11-13 21:27:34 +0100

    Initial commit

---
+++ button.js
@@ -0,0 +1,12 @@
+function showBtn() {
+    let x = document.getElementById("img")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
+
+function Alert() {
+    alert("Why did you click me?");
+}
